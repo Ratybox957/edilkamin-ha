972ea14bf634401a0a1f7dc2548db0cb5fde9702
Ratybox957 <47273966+Ratybox957@users.noreply.github.com>
2023-10-18 15:03:18 +0200

    Update climate.py

--- custom_components/edilkaminV2/climate.py
+++ custom_components/edilkaminV2/climate.py
@@ -7,6 +7,7 @@ from homeassistant.components.climate import (
     HVACMode,
     ClimateEntityFeature,
 )
+
 from homeassistant.const import TEMP_CELSIUS, ATTR_TEMPERATURE
 
 from .const import DOMAIN
@@ -36,7 +37,9 @@ class EdilkaminClimateEntity(ClimateEntity):
         self._current_temperature = None
         self._target_temperature = None
         self._fan1_speed = None
+        self._power_level = None
         self._hvac_mode = None
+        self._preset_mode = None
         self.api = api
         self.mac_address = api.get_mac_address()
         self._attr_max_temp = 24
@@ -78,6 +81,16 @@ class EdilkaminClimateEntity(ClimateEntity):
         return ["1", "2", "3", "4", "5"]
 
     @property
+    def preset_mode(self) :
+        """The current operation ."""
+        return self._preset_mode
+
+    @property
+    def preset_modes(self) :
+        """list of power level."""
+        return ["1", "2", "3", "4", "5"]
+
+    @property
     def target_temperature(self):
         """The current temperature."""
         return self._target_temperature
@@ -85,7 +98,7 @@ class EdilkaminClimateEntity(ClimateEntity):
     @property
     def supported_features(self):
         """Bitmap of supported features"""
-        return ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.FAN_MODE
+        return ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.PRESET_MODE
 
     async def async_set_fan_mode(self, fan_mode):
         """Set new target fan mode."""
@@ -109,12 +122,15 @@ class EdilkaminClimateEntity(ClimateEntity):
             self._current_temperature = await self.api.get_temperature()
             self._target_temperature = await self.api.get_target_temperature()
             self._fan1_speed = await self.api.get_fan_1_speed()
-
+            self._power_level = await self.api.get_actual_power()
+            self._preset_mode = self._power_level
+        
             power = await self.api.get_power_status()
             if power is True:
                 self._hvac_mode = HVACMode.HEAT
             else:
                 self._hvac_mode = HVACMode.OFF
+            
 
         except HttpException as err:
             _LOGGER.error(str(err))
@@ -129,13 +145,24 @@ class EdilkaminClimateEntity(ClimateEntity):
 
         if hvac_mode == HVACMode.OFF:
             return await self.async_turn_off()
-
         if hvac_mode == HVACMode.HEAT :
             return await self.async_turn_on()
+        
+        
 
         _LOGGER.info("Setting operation mode to %s", hvac_mode)
         self.async_write_ha_state()
 
+
+    async def async_set_preset_mode(self, preset_mode):
+        """Set new preset mode."""
+        _LOGGER.error(preset_mode)
+
+        if preset_mode not in ["1", "2", "3", "4", "5"]:
+            raise ValueError(f"Unsupported preset mode: {preset_mode}")
+        await self.async_set_power(preset_mode)
+        self.async_write_ha_state()
+        
     async def async_turn_on(self):
         """Turn on."""
         _LOGGER.debug("Turning %s on", self.unique_id)
@@ -147,3 +174,14 @@ class EdilkaminClimateEntity(ClimateEntity):
         _LOGGER.debug("Turning %s off", self.unique_id)
         await self.api.disable_power()
         self.async_write_ha_state()
+
+
+    async def async_set_power(self, power_level):
+        """Turn off."""
+        _LOGGER.debug("set power level %s", power_level)
+        try:
+            await self.api.set_power_level(power_level)
+        except HttpException as err:
+            _LOGGER.error(str(err))
+            return
+        
