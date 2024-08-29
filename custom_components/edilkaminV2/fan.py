6272a15eacb0246750ea60a7eda260f2b8e004c8
Ratybox957 <47273966+Ratybox957@users.noreply.github.com>
2024-08-29 15:04:51 +0200

    merge 2 fan in fan.py

--- custom_components/edilkaminV2/fan.py
+++ custom_components/edilkaminV2/fan.py
@@ -18,6 +18,7 @@ from custom_components.edilkaminv2.api.edilkamin_async_api import EdilkaminAsync
 _LOGGER = logging.getLogger(__name__)
 
 SPEED_RANGE = (1, 5)  # away is not included in speeds and instead mapped to off
+SPEED_RANGE_FAN2 = (0, 5)  # away is not included in speeds and instead mapped to off
 
 
 async def async_setup_entry(hass, config_entry, async_add_devices):
@@ -25,6 +26,7 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     async_api = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_devices([EdilkaminFan(async_api)])
+    async_add_devices([EdilkaminFan2(async_api)])
 
 
 class EdilkaminFan(FanEntity):
@@ -79,6 +81,75 @@ class EdilkaminFan(FanEntity):
             self.current_state = await self.api.get_power_status()
             if self.current_state is True:
                 self.current_speed = await self.api.get_fan_1_speed()
+        except HttpException as err:
+            _LOGGER.error(str(err))
+            return
+
+    async def async_turn_on(
+            self,
+            speed: str = None,
+            percentage: int = None,
+            preset_mode: str = None,
+            **kwargs,
+    ) -> None:
+        """Turn on the entity."""
+
+    async def async_turn_off(self, **kwargs) -> None:
+        """Turn off the entity."""
+
+
+class EdilkaminFan2(FanEntity):
+    """Representation of a Fan."""
+
+    def __init__(self, api: EdilkaminAsyncApi):
+        """Initialize the fan."""
+        self.api = api
+        self.mac_address = api.get_mac_address()
+
+        self.current_speed = None
+        self.current_state = False
+
+    @property
+    def unique_id(self):
+        """Return a unique_id for this entity."""
+        return f"{self.mac_address}_fan2"
+
+
+    @property
+    def percentage(self) -> int | None:
+        """Return the current speed percentage."""
+        if self.current_state is False:
+            return None
+
+        if self.current_speed is None:
+            return None
+        return ranged_value_to_percentage(SPEED_RANGE_FAN2, self.current_speed)
+
+    @property
+    def speed_count(self) -> int:
+        """Return the number of speeds the fan supports."""
+        return int_states_in_range(SPEED_RANGE_FAN2)
+
+    @property
+    def supported_features(self) -> int:
+        """Flag supported features."""
+        return SUPPORT_SET_SPEED
+
+    async def async_set_percentage(self, percentage: int) -> None:
+        """Set the speed of the fan, as a percentage."""
+        self.current_speed = math.ceil(
+            percentage_to_ranged_value(SPEED_RANGE_FAN2, percentage)
+        )
+
+        await self.api.set_fan_2_speed(self.current_speed)
+        self.schedule_update_ha_state()
+
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor."""
+        try:
+            self.current_state = await self.api.get_power_status()
+            if self.current_state is True:
+                self.current_speed = await self.api.get_fan_2_speed()
         except HttpException as err:
             _LOGGER.error(str(err))
             return
