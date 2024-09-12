c3f9115e28bcd76bb7277af514a88d53489f36a9
Ratybox957 <47273966+Ratybox957@users.noreply.github.com>
2024-09-12 08:55:26 +0200

    ADD power control

--- custom_components/edilkaminV2/fan.py
+++ custom_components/edilkaminV2/fan.py
@@ -17,6 +17,7 @@ from custom_components.edilkaminv2.api.edilkamin_async_api import EdilkaminAsync
 
 _LOGGER = logging.getLogger(__name__)
 
+POWER_RANGE = (1, 5)  # away is not included in speeds and instead mapped to off
 SPEED_RANGE = (1, 5)  # off is not included in speeds and instead mapped to off
 SPEED_RANGE_FAN2 = (1, 5)  # off is not included in speeds and instead mapped to off
 
@@ -25,6 +26,7 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
     """Add sensors for passed config_entry in HA."""
     async_api = hass.data[DOMAIN][config_entry.entry_id]
 
+    async_add_devices([EdilkaminPowerLevel(async_api)])
     async_add_devices([EdilkaminFan(async_api)])
     async_add_devices([EdilkaminFan2(async_api)])
 
@@ -155,7 +157,12 @@ class EdilkaminFan2(FanEntity):
     async def async_set_preset_mode(self, preset_mode: str) -> None:
         """Set the preset mode of the fan."""
         self.current_speed = preset_mode
+        self.preset_mode = preset_mode
+        if preset_mode == "0":
+            self.current_state = False
         
+        await self.api.set_fan_2_speed(self.current_speed)
+        self.schedule_update_ha_state()
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor."""
@@ -163,6 +170,7 @@ class EdilkaminFan2(FanEntity):
             self.current_state = await self.api.get_power_status()
             if self.current_state is True:
                 self.current_speed = await self.api.get_fan_2_speed()
+                self.preset_mode = str(self.current_speed)
         except HttpException as err:
             _LOGGER.error(str(err))
             return
@@ -179,4 +187,75 @@ class EdilkaminFan2(FanEntity):
     async def async_turn_off(self, **kwargs) -> None:
         """Turn off the entity."""
         await self.api.set_fan_2_speed(0)
+        self.current_state = False
+        self.preset_mode = "0"
         self.schedule_update_ha_state()
+
+
+class EdilkaminPowerLevel(FanEntity):
+    """Representation of a Fan."""
+
+    def __init__(self, api: EdilkaminAsyncApi):
+        """Initialize the fan."""
+        self.api = api
+        self.mac_address = api.get_mac_address()
+
+        self.current_speed = None
+        self.current_state = False
+
+    @property
+    def unique_id(self):
+        """Return a unique_id for this entity."""
+        return f"{self.mac_address}_power_level_1"
+
+
+    @property
+    def percentage(self) -> int | None:
+        """Return the current speed percentage."""
+        if self.current_state is False:
+            return None
+
+        if self.current_speed is None:
+            return None
+        return ranged_value_to_percentage(POWER_RANGE, self.current_speed)
+
+    @property
+    def speed_count(self) -> int:
+        """Return the number of speeds the fan supports."""
+        return int_states_in_range(POWER_RANGE)
+
+    @property
+    def supported_features(self) -> int:
+        """Flag supported features."""
+        return FanEntityFeature.SET_SPEED
+
+    async def async_set_percentage(self, percentage: int) -> None:
+        """Set the speed of the fan, as a percentage."""
+        self.current_speed = math.ceil(
+            percentage_to_ranged_value(POWER_RANGE, percentage)
+        )
+
+        await self.api.set_power_level(self.current_speed)
+        self.schedule_update_ha_state()
+
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor."""
+        try:
+            self.current_state = await self.api.get_power_status()
+            if self.current_state is True:
+                self.current_speed = await self.api.get_actual_power()
+        except HttpException as err:
+            _LOGGER.error(str(err))
+            return
+
+    async def async_turn_on(
+            self,
+            speed: str = None,
+            percentage: int = None,
+            preset_mode: str = None,
+            **kwargs,
+    ) -> None:
+        """Turn on the entity."""
+
+    async def async_turn_off(self, **kwargs) -> None:
+        """Turn off the entity."""
